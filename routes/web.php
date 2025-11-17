a7e14eb161bd4960647dd044375d8ab347a29459
Ray-Si-Arga <argaa3093@gmail.com>
2025-11-17 11:25:23 +0700

    Tambah Route Hutang

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     // Vendor
     Route::get('/vendor', function () {
-        return view('vendor.index');})->name('vendor.index');
+        return view('vendor.index');
+    })->name('vendor.index');
 
     //Ke halaman create vendor
     Route::get('/vendor/create', function () {
-        return view('vendor.store');})->name('vendor.create');
+        return view('vendor.store');
+    })->name('vendor.create');
 
     //Ketika Submit Akan Menjalankan Method Store di VendorController
     Route::post('/vendor/create', [VendorController::class, 'store'])->name('vendor.store');
@@ -40,12 +42,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     //Ketika Submit Akan Menjalankan Method Update di VendorController
     Route::put('/vendor/{id}', [VendorController::class, 'update'])->name('vendor.update');
 
-//=========================================================================================================
-// Produk 
+    //=========================================================================================================
+    // Produk 
 
     //Ke halaman create Produk
     Route::get('/produk/create', function () {
-        return view('produk.store');})->name('produk.create');
+        return view('produk.store');
+    })->name('produk.create');
 
     //Ketika Submit Akan Menjalankan Method Store di ProdukController
     Route::post('/produk/create', [ProductController::class, 'store'])->name('produk.store');
@@ -53,26 +56,29 @@ Route::middleware(['auth', 'verified'])->group(function () {
     //Ke halaman edit Produk
     Route::get('/produk/{id}/edit', [ProductController::class, 'edit'])->name('produk.edit');
 
-//Ketika Submit Akan Menjalankan Method Update di ProdukController
-Route::put('/produk/{id}', [ProductController::class, 'update'])->name('produk.update');
+    //Ketika Submit Akan Menjalankan Method Update di ProdukController
+    Route::put('/produk/{id}', [ProductController::class, 'update'])->name('produk.update');
 
-//memanggil resource controller untuk produk
-Route::resource('produk', ProductController::class);
+    //memanggil resource controller untuk produk
+    Route::resource('produk', ProductController::class);
 
     //=========================================================================================================
     // Stok Terkini
 
     Route::get('/stok_terkini', function () {
-        return view('stok_terkini.index');})->name('stok_terkini.index');
+        return view('stok_terkini.index');
+    })->name('stok_terkini.index');
 
     //=========================================================================================================
     // Barang Masuk
     Route::get('/barang_masuk', function () {
-        return view('barang_masuk.index');})->name('barang_masuk.index');
+        return view('barang_masuk.index');
+    })->name('barang_masuk.index');
 
     //Ke halaman create Barang Masuk
     Route::get('/barang_masuk/create', function () {
-        return view('barang_masuk.store');})->name('barang_masuk.create');
+        return view('barang_masuk.store');
+    })->name('barang_masuk.create');
 
     //Ketika Submit Akan Menjalankan Method Store di BarangMasukController
     Route::post('/barang_masuk/create', [Barang_MasukController::class, 'store'])->name('barang_masuk.store');
@@ -89,16 +95,18 @@ Route::resource('produk', ProductController::class);
     //=========================================================================================================
     // Barang Keluar
     Route::get('/barang_keluar', function () {
-        return view('barang_keluar.index');})->name('barang_keluar.index');
+        return view('barang_keluar.index');
+    })->name('barang_keluar.index');
 
     //Ke halaman create Barang Keluar
     Route::get('/barang_keluar/create', [Barang_KeluarController::class, 'create'])
-    ->name('barang_keluar.create');   // ✔ BENAR
+        ->name('barang_keluar.create');   // ✔ BENAR
 
-        
+
 
     Route::get('/barang_keluar/edit', function () {
-        return view('barang_keluar.edit');});
+        return view('barang_keluar.edit');
+    });
 
     //Ketika Submit Akan Menjalankan Method Store di BarangKeluarController
     Route::post('/barang_keluar/create', [Barang_KeluarController::class, 'store'])->name('barang_keluar.store');
@@ -112,11 +120,13 @@ Route::resource('produk', ProductController::class);
     //=========================================================================================================
     // Riwayat Transaksi
     Route::get('/riwayat_transaksi', function () {
-        return view('riwayat_transaksi.index');})->name('riwayat_transaksi.index');
+        return view('riwayat_transaksi.index');
+    })->name('riwayat_transaksi.index');
 
     //Ke halaman create Barang Keluar
     Route::get('/riwayat_transaksi/create', function () {
-        return view('riwayat_transaksi.store');})->name('riwayat_transaksi.create');
+        return view('riwayat_transaksi.store');
+    })->name('riwayat_transaksi.create');
 
     //Ketika Submit Akan Menjalankan Method Store di BarangKeluarController
     Route::post('/riwayat_transaksi/create', [FinancialTransactionController::class, 'store'])->name('riwayat_transaksi.store');
@@ -131,13 +141,25 @@ Route::resource('produk', ProductController::class);
     Route::get('/laporan-keuangan-pdf', [FinancialTransactionController::class, 'generatePDF'])->name('laporan.keuangan.pdf');
 
     //=========================================================================================================
+    // Hutang
+    route::get('/hutang', function () {
+        return view('ledger_entries.index');
+    })->name('ledger_entries.index');
+
+    Route::get('/hutang/create', function () {
+        return view('ledger_entries.store');
+    });
+
+    //=========================================================================================================
     // Pengguna
     Route::get('/pengguna', function () {
-        return view('pengguna.index');})->name('pengguna.index');
+        return view('pengguna.index');
+    })->name('pengguna.index');
 
     //Ke halaman create Barang Keluar
     Route::get('/pengguna/create', function () {
-        return view('pengguna.store');})->name('pengguna.create');
+        return view('pengguna.store');
+    })->name('pengguna.create');
 
     //Ketika Submit Akan Menjalankan Method Store di BarangKeluarController
     Route::post('/pengguna/create', [ProfileController::class, 'store'])->name('pengguna.store');
@@ -149,4 +171,4 @@ Route::resource('produk', ProductController::class);
     Route::put('/pengguna/{id}', [ProfileController::class, 'update'])->name('pengguna.update');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
